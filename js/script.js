eae6ad8557f4a457b8d35d1874f332dac28af7ee
Eduard Gorshkov <britvahp@gmail.com>
2021-03-07 01:23:50 +0400

    section stock

--- js/script.js
+++ js/script.js
@@ -13,51 +13,92 @@ window.addEventListener('DOMContentLoaded', () => {
   let counterForSwipe = 0;
   let counter = 0;
 
+  //Свайп табов с прогрессом
+
+  function swipeProgress(slWind, slField, slSlides, slProgress, slideWidth) {
+    const sliderProgressWind = document.querySelector(slWind),
+          sliderProgressField = document.querySelector(slField),
+          sliderProgressSlides = document.querySelectorAll(slSlides),
+          sliderProgress = document.querySelector(slProgress),
+          progressStep = 100 / sliderProgressSlides.length;
+
+    let sliderProgressFieldPos;
+    let touchProgressStart;
+    let touchProgressEnd;
+    let counterSwipeProgress = 1;
+
+    sliderProgress.style.width = `${progressStep}%`;
+
+    sliderProgressWind.addEventListener('touchstart', (e) => {
+      touchProgressStart = e.changedTouches[0].pageX;
+    });
+  
+    sliderProgressWind.addEventListener('touchmove', (e) => {
+      sliderProgressFieldPos = window.getComputedStyle(sliderProgressField).position;
+      if (sliderProgressFieldPos == 'absolute') {
+        let touchProgressMove;
+        touchProgressMove = e.changedTouches[0].pageX - touchProgressStart;
+        sliderProgressField.style.transform = `translateX(${touchProgressMove - slideWidth * (counterSwipeProgress - 1)}px)`;
+      } else {
+        sliderProgressField.style.transform = `translateX(0px)`;
+      }
+    });
+  
+    sliderProgressWind.addEventListener('touchend', (e) => {
+      if (sliderProgressFieldPos == 'absolute') {
+        touchProgressEnd = e.changedTouches[0].pageX - touchProgressStart;
+        if (Math.abs(touchProgressEnd) > 150) {
+          if (touchProgressEnd < 0) {
+            sliderProgressField.style.transform = `translateX(-${slideWidth * counterSwipeProgress}px)`;
+            counterSwipeProgress++;
+            if (counterSwipeProgress >= sliderProgressSlides.length) {
+              sliderProgressField.style.transform = `translateX(-${slideWidth * (sliderProgressSlides.length - 1)}px)`;
+              counterSwipeProgress = sliderProgressSlides.length;
+            }   
+          } else {
+              counterSwipeProgress--;
+              sliderProgressField.style.transform = `translateX(-${slideWidth * (counterSwipeProgress - 1)}px)`;
+              if (counterSwipeProgress <= 1) {
+              sliderProgressField.style.transform = `translateX(0px)`;
+              counterSwipeProgress = 1;
+            }
+          }
+        } else {
+          sliderProgressField.style.transform = `translateX(-${slideWidth * (counterSwipeProgress - 1)}px)`;
+        }
+      }
+      sliderProgress.style.width = `${progressStep * counterSwipeProgress}%`;
+    });
+  }
+
+  swipeProgress('.stock__wrap', '.stock__row', '.stock__card', '.progress__inner', 270);
 
   // свайп табов в секции slider
-
-  const move = function(e) {
-    if (sliderFieldWidth + 20 > document.documentElement.clientWidth) {
-      console.log('rdf');
-      let touchMove;
-      touchMove = e.changedTouches[0].pageX - touchStart;
-      sliderField.style.transform = `translateX(${touchMove + counterForSwipe}px)`;
-    } else {
-      sliderField.style.transform = `translateX(0px)`;
-    }
-  };
-  
-  // window.addEventListener('resize', () => {
-  //   if (document.documentElement.clientWidth <= 715) {
-  //     swipeTabs();
-  //   } else {
-  //     sliderField.style.transform = `translateX(0px)`;
-  //     sliderWind.removeEventListener('touchmove', move);
-  //   }
-  // });
-
-  // if (document.documentElement.clientWidth <= 715) {
-  //   swipeTabs();
-  // }
 
   function swipeTabs() {
     sliderWind.addEventListener('touchstart', (e) => {
       touchStart = e.changedTouches[0].pageX;
     });
   
-    sliderWind.addEventListener('touchmove', move);
+    sliderWind.addEventListener('touchmove', (e) => {
+      if (sliderFieldWidth + 10 > document.documentElement.clientWidth) {
+        let touchMove;
+        touchMove = e.changedTouches[0].pageX - touchStart;
+        sliderField.style.transform = `translateX(${touchMove + counterForSwipe}px)`;
+      } else {
+        sliderField.style.transform = `translateX(0px)`;
+      }
+    });
   
     sliderWind.addEventListener('touchend', (e) => {
       let clientWidth = document.documentElement.clientWidth;
       touchEnd = e.changedTouches[0].pageX - touchStart;
       counterForSwipe += touchEnd;
-      console.log(touchEnd);
-      console.log(sliderFieldWidth - clientWidth);
-      if (sliderField.getBoundingClientRect().right < clientWidth - 10 || (touchEnd < 0 && Math.abs(touchEnd) > sliderFieldWidth - 150)) {
+      if (sliderField.getBoundingClientRect().right < clientWidth || (touchEnd < 0 && Math.abs(touchEnd) > Math.abs(sliderField.getBoundingClientRect().right) - clientWidth)) {
         sliderField.style.transform = `translateX(-${sliderFieldWidth - clientWidth + 20}px)`;
         counterForSwipe = -(sliderFieldWidth - clientWidth + 10);
       }
-      if (sliderField.getBoundingClientRect().left > 10 || (touchEnd > 0 && Math.abs(touchEnd) > sliderFieldWidth - 150)) {
+      if (sliderField.getBoundingClientRect().left > 0 || (touchEnd > 0 && Math.abs(touchEnd) > Math.abs(sliderField.getBoundingClientRect().left))) {
         sliderField.style.transform = `translateX(0px)`;
         counterForSwipe = 0;
       }
