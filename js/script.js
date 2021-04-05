c143aca1250be61a0531a0832b78051f1510de61
Eduard Gorshkov <britvahp@gmail.com>
2021-04-05 17:49:09 +0400

    fix section path

--- js/script.js
+++ js/script.js
@@ -27,6 +27,68 @@ window.addEventListener('DOMContentLoaded', () => {
         cabHistAccordContents = document.querySelectorAll('.cab-hist__accord-content'),
         addMenu = document.querySelector('.add-menu'),
         addMenuBurger = document.querySelector('.add-menu__burger');
+
+
+
+  // Тренировка 
+
+
+  // const mask = (selector) => {
+  
+  //   let setCursorPosition = (pos, elem) => {
+  //     elem.focus();
+
+  //     if (elem.setSelectionRange) {
+  //       elem.setSelectionRange(pos, pos);
+  //     } else if (elem.createTextRange) {
+  //       let range = elem.createTextRange();
+
+  //       range.collapse(true);
+  //       range.moveEnd('character', pos);
+  //       range.moveStart('character', pos);
+  //       range.select();
+  //     }
+  //   };
+
+  //   const createMask = function(event) {
+  //     let matrix = '+7 (___) ___ __ __',
+  //         i = 0,
+  //         def = matrix.replace(/\D/g, ''),
+  //         val = this.value.replace(/\D/g, '');
+
+  //     if (def.length >= val.length) {
+  //       val = def;
+  //     }
+
+  //     if (val[1] == 9) {
+  //       this.value = matrix.replace(/./g, function(a) {
+  //         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+  //       });
+  //     } else {
+  //       this.value = '+7';
+  //     }
+      
+
+  //     if (event.type == 'blur') {
+  //       if (this.value.length == 2) {
+  //         this.value = '';
+  //       }
+  //     } else {
+  //       setCursorPosition(this.value.length, this);
+  //     }
+  //   };
+
+  //   let inputs = document.querySelectorAll(selector);
+
+  //   inputs.forEach(input => {
+  //     input.addEventListener('input', createMask);
+  //     input.addEventListener('focus', createMask);
+  //     input.addEventListener('blur', createMask);
+  //   });
+
+  // };
+
+  // mask('[type="tel"]');
         
 
         
@@ -70,7 +132,6 @@ window.addEventListener('DOMContentLoaded', () => {
           });
         }
       });
-      console.log(delay);
     }
   }
 
