dc8e32625ed712f1c77951d7a5fd2b48447205d3
BRITVA-hp <borbritvahp@gmail.com>
2022-02-02 20:57:33 +0400

    correction

--- js/script.js
+++ js/script.js
@@ -31,62 +31,72 @@ window.addEventListener('DOMContentLoaded', () => {
   // Тренировка 
 
 
-  // const mask = (selector) => {
+  const mask = (selector) => {
+
+    let inputs = document.querySelectorAll(selector);
   
-  //   let setCursorPosition = (pos, elem) => {
-  //     elem.focus();
+    let setCursorPosition = (pos, elem) => {
+      elem.focus();
 
-  //     if (elem.setSelectionRange) {
-  //       elem.setSelectionRange(pos, pos);
-  //     } else if (elem.createTextRange) {
-  //       let range = elem.createTextRange();
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        let range = elem.createTextRange();
 
-  //       range.collapse(true);
-  //       range.moveEnd('character', pos);
-  //       range.moveStart('character', pos);
-  //       range.select();
-  //     }
-  //   };
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
+    };
 
-  //   const createMask = function(event) {
-  //     let matrix = '+7 (___) ___ __ __',
-  //         i = 0,
-  //         def = matrix.replace(/\D/g, ''),
-  //         val = this.value.replace(/\D/g, '');
+    const createMask = function(event) {
+      let matrix = '+7 (___) ___ __ __',
+          i = 0,
+          def = matrix.replace(/\D/g, ''),
+          val = this.value.replace(/\D/g, '');
 
-  //     if (def.length >= val.length) {
-  //       val = def;
-  //     }
+      if (def.length >= val.length) {
+        val = def;
+      }
 
-  //     if (val[1] == 9) {
-  //       this.value = matrix.replace(/./g, function(a) {
-  //         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-  //       });
-  //     } else {
-  //       this.value = '+7';
-  //     }
+      // if (val[1] == 9) {
+        this.value = matrix.replace(/./g, function(a) {
+          // console.log(val);
+          // return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+          if (/[_\d]/.test(a) && i < val.length) {
+            console.log(1);
+            return val.charAt(i++);
+          } else if (i >= val.length) {
+            console.log(2);
+            return '';
+          } else  {
+            console.log(3);
+            return a;
+          }
+        });
+      // } else {
+      //   this.value = '+7';
+      // }
       
 
-  //     if (event.type == 'blur') {
-  //       if (this.value.length == 2) {
-  //         this.value = '';
-  //       }
-  //     } else {
-  //       setCursorPosition(this.value.length, this);
-  //     }
-  //   };
+      if (event.type == 'blur') {
+        if (this.value.length == 2) {
+          this.value = '';
+        }
+      } else {
+        setCursorPosition(this.value.length, this);
+      }
+    };
 
-  //   let inputs = document.querySelectorAll(selector);
+    inputs.forEach(input => {
+      input.addEventListener('input',createMask);
+      input.addEventListener('focus', createMask);
+      input.addEventListener('blur', createMask);
+    });
+  };
 
-  //   inputs.forEach(input => {
-  //     input.addEventListener('input', createMask);
-  //     input.addEventListener('focus', createMask);
-  //     input.addEventListener('blur', createMask);
-  //   });
-
-  // };
-
-  // mask('[type="tel"]');
+  mask('[type="tel"]');
   
 
   //Вспомогательные переменные
